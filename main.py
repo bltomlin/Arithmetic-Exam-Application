dfb181537b28e15558ebe7c32a9ed787bab69c1d
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-05 21:14:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-equation = input().split()
+import random
+# write your code here
+def add(x, y):
+    return x + y
+    
+def sub(x, y):
+    return x - y
+    
+def multiply(x, y):
+    return x * y
+    
+x = random.randint(2,9)
+y = random.randint(2,9)
+operator = ['+', '*', '-']
+operator = random.choice(operator)
+answer = 0
+
+equation = str(x) + operator + str(y)
 if equation[1] == '+':
-    answer = int(equation[0]) + int(equation[2])
-    print(answer)
+    answer = add(x, y)
 if equation[1] == '-':
-    answer = int(equation[0]) - int(equation[2])
-    print(answer)
+    answer = sub(x, y)
 if equation[1] == '*':
-    answer = int(equation[0]) * int(equation[2])
-    print(answer)
+    answer = multiply(x, y)
+print(' '.join(equation))
+user_answer = input()
+
+if user_answer == answer:
+    print('Right!')
+else:
+    print('Wrong!')
