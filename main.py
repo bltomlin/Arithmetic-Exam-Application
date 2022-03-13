73acd3c34a2be62c6ff32fb2e2e78e904afb954b
bltomlin <brendan.tomlinson2@gmail.com>
2022-03-12 22:54:13 -0800

    1.  The application should give the user 5 tasks. The tasks are akin to the previous stage: two numbers from 2 to 9 and an integer operation.   2.  The user receives one task, prints the answer. If the answer contains a typo (letters or otherwise empty), the program should print Incorrect format. and ask to re-enter the answer. Repeat until the answer is in the correct format. If the answer is a number, print Right! or Wrong! depending on the answer and carry on to the next question.   3.  After five tasks, output Your mark is n/5. where n is the number of correct answers.

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
-equation = input().split()
-if equation[1] == '+':
-    answer = int(equation[0]) + int(equation[2])
-    print(answer)
-if equation[1] == '-':
-    answer = int(equation[0]) - int(equation[2])
-    print(answer)
-if equation[1] == '*':
-    answer = int(equation[0]) * int(equation[2])
-    print(answer)
+import random
+
+#methods that will be used
+def add(x, y):
+    return x + y
+    
+def sub(x, y):
+    return x - y
+    
+def multiply(x, y):
+    return x * y
+
+
+#Variables are delcared
+answer = 0
+answer_count = 0
+
+#main
+for question_count in range(5):
+    user_answer = 0
+    x = random.randint(2,9)
+    y = random.randint(2,9)
+    operator = ['+', '*', '-']
+    operator = random.choice(operator)
+    equation = str(x) + operator + str(y)
+    if equation[1] == '+':
+        answer = add(x, y)
+    elif equation[1] == '-':
+        answer = sub(x, y)
+    else:
+        answer = multiply(x, y)
+    print(' '.join(equation))
+
+    #user inputs the answer
+    try:
+        user_answer = int(input())
+    except ValueError:
+        while not int(user_answer):
+            print('Incorrect format.')
+            try:
+                user_answer = int(input())
+            except ValueError:
+                continue
+        if user_answer == answer:
+            print('Right!')
+            answer_count += 1
+        else:
+            print('Wrong')
+    else:
+        #Output of program
+        if user_answer == answer:
+            print('Right!')
+            answer_count += 1
+        else:
+            print('Wrong!')
+print('Your mark is ' + str(answer_count) + '/5')
