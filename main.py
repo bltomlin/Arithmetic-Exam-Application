77c51c4735168e1ae8f2177f13ade15d225deff6
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-19 02:20:55 -0700

    Final project
    Fully functional and tested rigorously. A few changes to make global variables recognized in methods due to local variable references errors being caught.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
-#methods that will be used
+user_answer = 0
+answer_count = 0
 
 #adds two numbers
 def add(x, y):
@@ -14,7 +15,9 @@ def sub(x, y):
 def multiply(x, y):
     return x * y
 
+#user selects difficulty
 def difficulty():
+    global user_answer
     print('Which level do you want? Enter a number:')
     print('1 - simple operations with numbers 2-9')
     print('2 - integral squares of 11-29')
@@ -26,24 +29,25 @@ def difficulty():
             try:
                 user_answer = int(input())
                 if (user_answer == 1) or (user_answer == 2):
-                    return user_answer
+                    return
             except ValueError:
                 continue
     else:
         if (user_answer == 1) or (user_answer == 2):
-            return user_answer
+            return
         else:
             while (user_answer != 1) or (user_answer != 2):
                 print('Incorrect format.')
                 try:
                     user_answer = int(input())
                     if (user_answer == 1) or (user_answer == 2):
-                        return user_answer
+                        return
                 except ValueError:
                     continue
 
+#5 pseudo-random questions on math sum, difference and multiplcation.
 def simple_ops():
-    answer_count = 0
+    global answer_count
     for question_count in range(5):             #asks 5 questions
         user_answer = 0
         x = random.randint(2,9)                 #sets variables used in question
@@ -85,10 +89,11 @@ def simple_ops():
                 answer_count += 1
             else:
                 print('Wrong!')
-    print('Your mark is ' + str(answer_count) + '/5')
+    print('Your mark is ' + str(answer_count) + '/5. Would you like to save your result to the file? Enter yes or no.')
 
+#5 pseudo_random questions on squaring numbers
 def int_squares():
-    answer_count = 0
+    global answer_count
     for question_count in range(5):             #asks 5 questions
         user_answer = 0
         x = random.randint(11,29) 
@@ -118,7 +123,43 @@ def int_squares():
                 answer_count += 1
             else:
                 print('Wrong!')
-    print('Your mark is ' + str(answer_count) + '/5')
+    print('Your mark is ' + str(answer_count) + '/5. Would you like to save your result to the file? Enter yes or no.')
+    return answer_count
 
 
-int_squares()
+#asking user if they want to save the final score
+def save_score(answer_count, user_answer):
+    level_desc = ''
+    if user_answer == 1:
+        level_desc = 'simple operations with numbers 2-9'
+    elif user_answer == 2:
+        level_desc = 'integral squares 11-29'
+    else:
+        print('Program is broken')
+        quit()
+    user_input = str(input())
+    if user_input.lower() == 'yes' or user_input == 'y':
+        print('What is your name?')
+        user_name = str(input())
+        results = open('results.txt', 'w', encoding="utf-8")
+        results.write(f'{user_name}: {answer_count}/5 in level {user_answer} ({level_desc}).')
+        results.close()
+        print('The results are saved in "results.txt".')
+    else:
+        exit()
+
+
+def main():
+    global answer_count
+    global user_answer
+    difficulty()
+    if user_answer == 1:
+        simple_ops()
+    elif user_answer == 2:
+        int_squares()
+    else:
+        print('Something is wrong')
+        exit()
+    save_score(answer_count, user_answer)
+
+main()
